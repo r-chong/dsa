297e35e71d2034fddd168552276be4902a97ba52
r-chong <dev.reese.chong@gmail.com>
2026-05-13 06:59:52 +0200

    leetcode: 110. Balanced Binary Tree

---
+++ balanced-binary-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+
+        return max(1 + dfs(root->left), 1 + dfs(root->right));
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+
+        if (abs(dfs(root->left) - dfs(root->right)) > 1) {
+            return false;
+        }
+
+        return isBalanced(root->left) && isBalanced(root->right);
+    }
+};
