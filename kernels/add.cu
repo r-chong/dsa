78dfafd57641d361b7fb8272803e9fc2100722b9
r-chong <dev.reese.chong@gmail.com>
2026-07-23 22:50:06 +0200

    leetgpu: vector add

---
+++ kernels/add.cu
@@ -0,0 +1,19 @@
+#include <cuda_runtime.h>
+
+__global__ void vector_add(const float* A, const float* B, float* C, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < N) {
+        C[i] = A[i] + B[i];
+    }
+}
+
+// A, B, C are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* A, const float* B, float* C, int N) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    // Kernel launch syntax
+    vector_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+    cudaDeviceSynchronize();
+}
