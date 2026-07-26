dc2e38f8534d032877a8b5648632b96d1781743a
r-chong <dev.reese.chong@gmail.com>
2026-07-26 17:37:25 +0200

    leetcode: 39. Combination Sum

---
+++ combination-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    void backtrack(int i, int sum, vector<int> &current, vector<int> &candidates, int target, vector<vector<int>> &res) {
+        if (i == candidates.size()) {
+            if (sum == target) {
+                res.push_back(current);
+            }
+            return;
+        }
+        if (sum > target) {
+            return;
+        }
+        // exclude (move onto next number)
+        backtrack(i + 1, sum, current, candidates, target, res);
+
+        // include 1+ copies
+        current.push_back(candidates[i]);
+        backtrack(i, sum + candidates[i], current, candidates, target, res);
+        current.pop_back();
+    }
+public:
+    // TC: O(k), SC: O(n)
+    // n = number of elements in candidates
+    // k = number of subsets
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> current;
+        vector<vector<int>> res;
+
+        backtrack(0, 0, current, candidates, target, res);
+
+        return res;
+    }
+};
+// divergences:
+// - inserted arguments into pop_back
+// - copied format of the subsets problem, but this one you may reuse the number.
+// - couldn't figure out sum > target case
