32c286ae6c76962dcc577af20d35c5bb3947f01e
r-chong <dev.reese.chong@gmail.com>
2026-08-01 15:16:55 +0200

    leetcode: 98. Validate Binary Search Tree
    brute force

--- incorrect/validate-binary-search-tree.cpp
+++ incorrect/validate-binary-search-tree.cpp
@@ -9,20 +9,52 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// Brute force solution O(n^2)
 class Solution {
+    bool containsGEQ(TreeNode* node, TreeNode* root) {
+        if (node == nullptr) {
+            return false;
+        }
+
+        if (node->val >= root->val) return true;
+
+        return containsGEQ(node->left, root) || containsGEQ(node->right, root);
+    }
+
+    bool containsLEQ(TreeNode* node, TreeNode* root) {
+        if (node == nullptr) {
+            return false;
+        }
+
+        if (node->val <= root->val) return true;
+
+        return containsLEQ(node->left, root) || containsLEQ(node->right, root);
+    }
 public:
     bool isValidBST(TreeNode* root) {
         if (root == nullptr) {
             return true;
         }
+        bool validImmediate = false;
 
-        bool left = root->left == nullptr || root->left && root->left->val >= root->val;
-        bool right = root->right == nullptr || root->right && root->right->val < root->val;
-
-        if (!left || !right) {
-            return false;
+        if (root->left && root->right) {
+            validImmediate = root->left->val < root->val && root->right->val > root->val;
+        } else if (root->left) {
+            validImmediate = root->left->val < root->val;
+        } else if (root->right) {
+            validImmediate = root->right->val > root->val;
+        } else {
+            // both null
+            validImmediate = true;
         }
 
-        return isValidBST(root->left) && isValidBST(root->right);
+        bool containsInvalid = containsGEQ(root->left, root) || containsLEQ(root->right, root);
+
+        bool left = isValidBST(root->left);
+        bool right = isValidBST(root->right);
+
+        return left && right && validImmediate && !containsInvalid;
     }
 };
+// divergences:
+// - missed the containsInvalid case
