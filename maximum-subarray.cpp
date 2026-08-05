92b8bd19a6156b4aa85d50fa0eb49fa02625c3d7
r-chong <dev.reese.chong@gmail.com>
2026-08-05 19:10:15 +0200

    leetcode: 53. Maximum Subarray
    Kadane's

---
+++ maximum-subarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    // Kadane's: start new streak OR continue previous
+
+    // TC: O(n), SC: O(1)
+    // n = size of nums
+    // each node is processed once
+    int maxSubArrayKadanes(vector<int>& nums) {
+        int current = 0;
+        int best = nums[0];
+
+        for (int i = 0; i < nums.size(); i++) {
+            current += nums[i];
+
+            if (current < nums[i]) {
+                current = nums[i];
+            }
+
+            best = max(best, current);
+        }
+
+        return best;
+    }
+};
+// divergences:
+// - didn't know Kadane's algorithm
+// - set best as 0 when it should have started at nums[0] (edge case for nums.size() == 1)
