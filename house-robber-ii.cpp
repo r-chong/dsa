bba0c3541d2909dc5487dc87204631aed776b9d0
r-chong <dev.reese.chong@gmail.com>
2026-09-01 22:45:38 -0400

    leetcode: 213. House Robber II

--- house-robber-ii.cpp
+++ house-robber-ii.cpp
@@ -36,3 +36,30 @@ public:
 // - didn't force cases for the first value meaning "robFirst" didn't represent accurately
 // - didn't handle nums.size()==1
 // - forgot to clear memo before reusing for second case
+
+// cleaner sol:
+class Solution {
+    vector<vector<int>> memo;
+
+    int traverse(int i, bool robbedFirst, vector<int>& nums) {
+        if (i >= nums.size()) return 0;
+        if (memo[i][robbedFirst] != -1) return memo[i][robbedFirst];
+
+        int rob = 0;
+        if (i < nums.size() - 1 || i == nums.size() - 1 && !robbedFirst) {
+            rob = nums[i] + traverse(i + 2, robbedFirst, nums);
+        }
+        int skip = traverse(i + 1, robbedFirst, nums);
+
+        return memo[i][robbedFirst] = max(rob, skip);
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        memo = vector<vector<int>>(n, vector<int>(2, -1));
+
+        return max(traverse(1, false, nums), nums[0] + traverse(2, true, nums));
+    }
+};
+// divergences:
+// - did not apply robbing logic uniformly in parent rob() - do nums[0] + traverse ...
