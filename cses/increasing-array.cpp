03b955e6eaf10cef75e070b6dbff6a9d594b8716
r-chong <dev.reese.chong@gmail.com>
2026-09-14 16:20:37 -0400

    fixup! cses: Increasing Array

--- cses/increasing-array.cpp
+++ cses/increasing-array.cpp
@@ -5,18 +5,18 @@ int main() {
     int n;
     cin >> n;
 
-    long long prev = 1;
+    long long min = 1;
     long long x;
     long long minMoves = 0;
     
     while (cin >> x) {
-        long long delta = prev - x;
+        long long delta = min - x;
 
         if (delta > 0) {
             // if delta is positive, then we must increase to prev
             minMoves += delta;
         } else {
-            prev = x;
+            min = x;
         }
     }
 
@@ -24,4 +24,6 @@ int main() {
     return 0;
 }
 // divergences:
-// - only update prev when we have no difference
+// - only update prev in the else bc prev is supposed to represent the value of the previous element after you've made any required increases
+// - so if we update it on every move then we're setting a value that should not actually be the prev
+// - we're forcing a global minimum
