a5660d5d5372757cd843bc805f65c52e7554af3f
r-chong <dev.reese.chong@gmail.com>
2026-09-15 21:03:16 -0400

    cses: Permutations

---
+++ cses/permutations.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if ((n < 4) && (n != 1)) {
+        cout << "NO SOLUTION";
+        return 0;
+    }
+
+    if (n == 4) {
+        cout << "2 4 1 3 ";
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i += 2) {
+        cout << i << " ";
+    }
+
+    for (int i = 2; i <= n; i += 2) {
+        cout << i << " ";
+    }
+    
+    return 0;
+}
