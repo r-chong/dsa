130e7f166e85a2272b958cc176bb349d2397920d
r-chong <dev.reese.chong@gmail.com>
2026-09-22 10:50:01 -0400

    leetcode: 5. Longest Palindromic Substring

--- longest-palindrome.cpp
+++ longest-palindrome.cpp
@@ -48,3 +48,49 @@ public:
 // - didn't respect the two pointer solution. Thought DP would be the same as "palindromic substrings"
 // - just declare len at top of loop
 // - did a read of l,r after updating l,r causing OOB
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int bestLen = 1;
+        int bestStart = 0;
+
+        for (int center = 0; center < s.size(); center++) {
+            // odd center
+            int l = center;
+            int r = center;
+
+            while (l >= 0 && r < s.size() && (s[l] == s[r])) {
+                int len = r - l + 1;
+
+                if (len > bestLen) {
+                    bestLen = len;
+                    bestStart = l;
+                }
+
+                l--;
+                r++;
+            }
+
+            // even center
+            l = center;
+            r = center + 1;
+
+            while (l >= 0 && r < s.size() && (s[l] == s[r])) {
+                int len = r - l + 1;
+
+                if (len > bestLen) {
+                    bestLen = len;
+                    bestStart = l;
+                }
+
+                l--;
+                r++;
+            }
+        }
+
+        return s.substr(bestStart, bestLen);
+    }
+};
+// divergences:
+// - got ahead of myself; need to set bestLen and bestStart to the right items
